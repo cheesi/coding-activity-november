c3c473bed3faaac986b3e72a0083069a13915b29
Christian Schabetsberger <christian.schabetsberger@finxit.at>
2022-11-22 15:36:27 +0100

    number orderings

--- Refucktoring/Dummy1.cs
+++ Refucktoring/Dummy1.cs
@@ -6,12 +6,12 @@ namespace Refucktoring
     {
         static int[] MoreStupidEveryThing = new int[]
         {
-            70, 0x69, 0b01111010
+            0b01111010, 69, 70, 0x69, 420, 666
         };
 
         public static string Dummy1Method()
         {
-            var data = ((char)MoreStupidEveryThing[0]).ToString() + (char)MoreStupidEveryThing[1] + (char)MoreStupidEveryThing[2] + (char)MoreStupidEveryThing[2];
+            var data = ((char)MoreStupidEveryThing[2]).ToString() + (char)MoreStupidEveryThing[3] + (char)MoreStupidEveryThing[0] + (char)MoreStupidEveryThing[0];
             return data;
         }
     }
