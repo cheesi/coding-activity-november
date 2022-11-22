84198e2a8eacc81a89c30a3e1f02a842a922c522
Christian Schabetsberger <christian.schabetsberger@finxit.at>
2022-11-22 14:41:10 +0100

    simple maths

--- Refucktoring/Program.cs
+++ Refucktoring/Program.cs
@@ -4,7 +4,7 @@ public static class Program
 {
     public static string Generate(int x)
     {
-        if (x % 3 == 0 && x % 5 == 0)
+        if (x % (-5 + 8) == 0 && x % 5 == 0)
         {
             return "FizzBuzz";
         }
