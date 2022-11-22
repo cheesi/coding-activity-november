1d338249fea893ac860cf488c83bf155bf2aa0ee
Christian Schabetsberger <christian.schabetsberger@finxit.at>
2022-11-22 15:45:26 +0100

    magic string building

--- Refucktoring/Dummy2.cs
+++ Refucktoring/Dummy2.cs
@@ -1,6 +1,7 @@
 ﻿using Refucktoring;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,11 @@ namespace Rеfucktoring
 {
     internal class Dummy1 : Refucktoring.Dummy1
     {
+        const string alphabet = "asdgjksdivbm,nmdif";
+
         public static string Dummy1Method()
         {
-            return Refucktoring.Dummy1.Dummy1Method().Replace(typeof(Dummy1).Namespace[2].ToString().ToUpper(), "B").Replace("i", "u");
+            return Refucktoring.Dummy1.Dummy1Method().Replace(typeof(Dummy1).Namespace[2].ToString().ToUpper(), alphabet[10].ToString().ToUpper()).Replace(alphabet[8].ToString(), ((char)(alphabet[8]+12)).ToString() );
 
             return Refucktoring.Dummy1.Dummy1Method().Replace("lulululu", "bababababa").Replace("NaNNaNNaNNaNNaN", "Batmaaaaann");
         }
